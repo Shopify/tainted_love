97b0f32c50bc9c4247822c42a04b754484273968
Benoit Côté-Jodoin <benoit.cotejodoin@shopify.com>
2019-03-18 17:33:33 -0400

    Mark hexdigest to check if they end up in String#==

---
+++ lib/tainted_love/replacer/replace_digest.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module TaintedLove
+  module Replacer
+    class ReplaceDigest < Base
+      def should_replace?
+        Object.const_defined?('Digest')
+      end
+
+      def replace!
+        digests = [:MD5, :SHA256, :SHA512, :SHA2, :SHA384, :SHA1]
+
+        digests.each do |digest|
+          mod = Digest.const_get(digest)
+          mod.extend(create_module(mod)) if Object.const_defined?(mod.to_s)
+        end
+      end
+
+      def create_module(digest_class)
+        module_code = %Q{
+        Module.new do
+          def hexdigest(value)
+            digest = super
+
+            digest.instance_eval do
+              def tainted_love_origin
+                #{digest_class}
+              end
+            end
+
+            digest
+          end
+        end
+}
+        eval(module_code)
+      end
+    end
+  end
+end

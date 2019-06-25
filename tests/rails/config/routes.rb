828de799a60807825d5dfea1eb98015aefd9f887
Benoit Côté-Jodoin <benoit.cotejodoin@shopify.com>
2019-06-25 15:29:57 -0400

    Add route to Rails taint test case

--- tests/rails/config/routes.rb
+++ tests/rails/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'test_cases/unsafe_render'
   get 'test_cases/render_inline'
   get 'test_cases/unsafe_redirect'
+  get 'test_cases/taint_test/:route_param' => 'test_cases#taint_test', as: 'test_cases_taint_test'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
